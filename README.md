117e4b926ab1c32b08fbd7666d7d79dba726ba07
wangjohn <wangjohn@mit.edu>
2013-05-25 21:35:48 -0400

    First commit

--- README.md
+++
@@ -1,4 +0,0 @@
-chi_epsilon_website
-===================
-
-The website for MIT's Chi Epsilon chapter.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Www::Application.config.secret_token = '43fff6f24cb76ae03058e1e129c66a128e65ac12735f074e98cf994f6fd67a44c14d279d3fa562b21f078fff878dbd3476423951c77c24daa0bdddc8b64dfac4'
